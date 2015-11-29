c8f1175f45b8ac4536138116b62a90707a1fadf5
unknown <eljhkrr@gmail.com>
2015-11-29 11:58:47 +0300

    is_multiple tests

--- ch1.py
+++ ch1.py
@@ -1,8 +1,19 @@
+import unittest
+
 def is_multiple(n, m):
-    if n%m == 0:
+    if m % n == 0:
         return True
     else:
         return False
 
+class TestCh1(unittest.TestCase):
+
+    def test_ismultiple(self):
+        self.assertTrue(is_multiple(2, 4))
+        self.assertFalse(is_multiple(3, 4))
+        self.assertFalse(is_multiple(4, 2))
+        with self.assertRaises(ZeroDivisionError):
+            is_multiple(0, 10)
+
 if __name__ == '__main__':
-    pass
+    unittest.main()
