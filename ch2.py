d106f868b038a5e069f4a1de113b6dd4987099c8
unknown <eljhkrr@gmail.com>
2015-12-02 15:52:10 +0300

    more flower tests

--- ch2.py
+++ ch2.py
@@ -31,12 +31,18 @@ class TestCh2(unittest.TestCase):
 
     def test_name(self):
         self.assertEqual(self.f.get_name(), "rose")
+        self.f.set_name("cherry")
+        self.assertEqual(self.f.get_name(), "cherry")
 
     def test_petals(self):
         self.assertEqual(self.f.get_petals(), 6)
+        self.f.set_petals(10)
+        self.assertEqual(self.f.get_petals(), 10)
 
     def test_price(self):
         self.assertEqual(self.f.get_price(), 100)
+        self.f.set_price(200)
+        self.assertEqual(self.f.get_price(), 200)
 
 if __name__ == '__main__':
     unittest.main()
