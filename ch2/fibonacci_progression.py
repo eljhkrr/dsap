f249e50e1b68b95f3c51eff18ea1daffd43a7717
unknown <eljhkrr@gmail.com>
2015-12-06 17:50:34 +0300

    fibonacci progression

---
+++ ch2/fibonacci_progression.py
@@ -0,0 +1,17 @@
+from progression import Progression
+
+class FibonacciProgression(Progression):
+    """Iterator producing a generalized Fibonacci progression."""
+
+    def __init__(self, first=0, second=1):
+        """Create a new Fibonacci progression.
+
+        start   first item of the progression (default 0)
+        second  second item of the progression (default 1)
+        """
+        super().__init__(first)
+        self._prev = second - first
+
+    def _advance(self):
+        """Update current value by taking sum of previous two."""
+        self._prev, self._current = self._current, self._current + self._prev
