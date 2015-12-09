aab49d59c57c392452ac4568d5f00f46fa2b521d
unknown <eljhkrr@gmail.com>
2015-12-09 21:34:23 +0300

    prefix average 3

--- ch3/prefix_avg.py
+++ ch3/prefix_avg.py
@@ -14,5 +14,15 @@ def prefix_average2(S):
     n = len(S)
     A = [0] * n
     for j in range(n):
-        A[j] = sum(S[0:j+1)) / (j+1)
+        A[j] = sum(S[0:j+1]) / (j+1)
+    return A
+
+def prefix_average3(S):
+    """Return list such that, for all j, A[j] equals average of S[0]..S[j]."""
+    n = len(S)
+    A = [0] * n
+    total = 0
+    for j in range(n):
+        total += S[j]
+        A[j] = total / (j+1)
     return A
