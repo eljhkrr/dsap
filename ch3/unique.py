64acf2be8c2d6911007b1319cf0c70d27270a630
unknown <eljhkrr@gmail.com>
2015-12-09 22:39:38 +0300

    unique 2

--- ch3/unique.py
+++ ch3/unique.py
@@ -5,3 +5,11 @@ def unique1(S):
             if S[j] == S[k]:
                 return False
     return True
+
+def unique2(S):
+    """Return True if there are no duplicate elements in sequence S."""
+    temp = sorted(S)
+    for j in range(1, len(temp)):
+        if S[j-1] == S[j]:
+            return False
+    return True
