694266dd101aaca0a0d735f88b2cff94deed7033
unknown <eljhkrr@gmail.com>
2015-12-09 22:25:20 +0300

    disjoint 2

--- ch3/disjoint.py
+++ ch3/disjoint.py
@@ -6,3 +6,13 @@ def disjoint1(A, B, C):
                 if a == b == c:
                     return False
     return True
+
+def disjoint2(A, B, C):
+    """Return True if there is no element common to all 3 lists."""
+    for a in A:
+        for b in B:
+            if a == b:
+                for c in C:
+                    if a == c:
+                        return False
+    return True
