a80bac9ebf0b840f39d1ac69846bc746f1fa5f3b
unknown <eljhkrr@gmail.com>
2015-12-10 15:54:07 +0300

    draw_interval

--- ch4/line.py
+++ ch4/line.py
@@ -4,3 +4,10 @@ def draw_line(tick_length, tick_label=''):
     if tick_label:
         line += ' ' + tick--label
     print(line)
+
+def draw_interval(center_length):
+    """Draw tick interval based upon a central tick length."""
+    if center_length > 0:
+        draw_interval(center_length - 1)
+        draw_line(center_length)
+        draw_interval(center_length - 1)
