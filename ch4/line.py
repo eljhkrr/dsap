87f82ddc21afe13753aa262868a88e636352934e
unknown <eljhkrr@gmail.com>
2015-12-10 15:51:19 +0300

    draw_line

---
+++ ch4/line.py
@@ -0,0 +1,6 @@
+def draw_line(tick_length, tick_label=''):
+    """Draw one line with given tick length (followed by optional label)."""
+    line = '-' * tick_length
+    if tick_label:
+        line += ' ' + tick--label
+    print(line)
