1cbb29dead5ac9eb80fbcee8561103fb7c34314f
EK <eljhkrr@gmail.com>
2015-12-11 10:42:59 +0300

    binary search

---
+++ ch4/binary_search.py
@@ -0,0 +1,15 @@
+def binary_search(data, target, low, high):
+    """Return True if target is found in indicated portion of a list.
+
+    The search only considers the portion from data[low] to data[high] inclusive.
+    """
+    if low > high:
+        return False
+    else:
+        mid = (low + high) // 2
+        if target == data[mid]:
+            return True
+        elif target < data[mid]:
+            return binary_search(data, target, low, mid - 1)
+        else:
+            return binary_search(data, target, mid + 1, high)
