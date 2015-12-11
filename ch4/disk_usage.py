23833e42a0fc5ab8a57196657e370915dbb822f5
EK <eljhkrr@gmail.com>
2015-12-11 11:16:54 +0300

    disk usage

---
+++ ch4/disk_usage.py
@@ -0,0 +1,11 @@
+import os
+
+def disk_usage(path):
+    """Return the number of bytes used by a file/folder and any descendants."""
+    total = os.path.getsize(path)
+    if os.path.isdir(path):
+        for filename in os.listdir(path):
+            childpath = os.path.join(path, filename)
+            total += disk_usage(childpath)
+    print('{0:<7}'.format(total), path)
+    return total
